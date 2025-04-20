05d02ca97336214c537226f223be8c60594cba3a
simuneu <worksimu@gmail.com>
2025-04-20 22:21:34 +0900

    js-sentence, img api / iframe, component 이동

---
+++ js/0420/06-component2-diary.js
@@ -0,0 +1,6 @@
+const diaryMenu=`
+    <div>일기목록</div>
+    <div>일기1</div>
+    <div>일기2</div>
+    <div>....</div>
+`
---
+++ js/0420/06-component3-dog.js
@@ -0,0 +1,6 @@
+const dogMenu=`
+    <div>강아지목록</div>
+    <div>강아지1</div>
+    <div>강아지2</div>
+    <div>....</div>
+`
